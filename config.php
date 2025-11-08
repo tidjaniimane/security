cfc49b318f2aca87d8fd711b767fdffca4dae2b5
tidjaniimane <imanetidjani1174@gmail.com>
2025-11-08 06:55:51 -1100

    Refactor database connection credentials in config.php

--- config.php
+++ config.php
@@ -1,12 +1,11 @@
 <?php
 $host = "localhost";
-$user = "root";     // your WAMP/MAMP username
-$pass = "";         // your WAMP/MAMP password (empty by default)
 $db   = "security";
+$user = "root"; // default WAMP user
+$pass = "";     // default WAMP password is empty
 
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-?>
