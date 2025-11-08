f46df2d2746780bf9493fe259162b6d76eb40d3d
tidjaniimane <imanetidjani1174@gmail.com>
2025-11-08 06:38:20 -1100

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,14 @@
 <?php
-$host   = getenv('DB_HOST');
-$user   = getenv('DB_USER');
-$pass   = getenv('DB_PASS');
-$dbname = getenv('DB_NAME');
+$host = getenv('DB_HOST');
+$db   = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$port = getenv('DB_PORT');
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected successfully"; // optional test
 } catch(PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
