0a3faece0d32ac4e7c9baebd4dfa8e81e5b1e441
tidjaniimane <imanetidjani1174@gmail.com>
2025-11-08 07:05:25 -1100

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $db   = "security";
 $user = "root"; // default WAMP user
 $pass = "";     // default WAMP password is empty
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysql($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
