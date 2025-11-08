d396bb709af082c13a33bff0c9459f37bbe4cf42
tidjaniimane <imanetidjani1174@gmail.com>
2025-11-08 06:23:07 -1100

    Update database connection environment variables

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$host = getenv("localhost");
-$user = getenv("root");
-$pass = getenv("");
-$dbname = getenv("security");
+$host   = getenv('DB_HOST');
+$user   = getenv('DB_USER');
+$pass   = getenv('DB_PASS');
+$dbname = getenv('DB_NAME');
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
@@ -10,4 +10,3 @@ try {
 } catch(PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
-?>
